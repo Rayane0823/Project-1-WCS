ffb9a8e8bb9117f12727694c4d15ba7901ff5ca5
Romain <98345704+Romlion@users.noreply.github.com>
2023-09-29 13:35:54 +0200

    I changed line in html and css

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const timerElement = document.getElementById("timer");
 let timer = 15;
 
 function timeDecreasing() {
-    timerElement.innerText = timer;
-    timer--;
+  timerElement.innerText = timer;
+  timer--;
 
-    if (timer <= 0) {
-        timer = 0;
-    }
+  if (timer <= 0) {
+    timer = 0;
+  }
 }
 
 setInterval(timeDecreasing, 1000);
