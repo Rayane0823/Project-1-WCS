f295c554d87493386cc7cb4d02d4978c37b4d874
Romain <98345704+Romlion@users.noreply.github.com>
2023-10-04 15:44:45 +0200

    I rectified lines in html, css and JS

--- Question.js
+++ Question.js
@@ -1,104 +1,104 @@
-export const questionsToQuiz = {
-  question: "quelle est la population actuelle de Lille?",
+// export const questionsToQuiz = {
+//   question: "quelle est la population actuelle de Lille?",
 
-  answers: {
-    answerA: "300 000 habitants",
-    answerB: "500 000 habitants",
-    answerC: "800 000 habitants",
-    answerD: "1 million d'habitants",
-  },
-  rightAnswer: "answerB",
+//   answers: {
+// answerA: "300 000 habitants",
+// answerB: "500 000 habitants",
+// answerC: "800 000 habitants",
+// answerD: "1 million d'habitants",
+//   },
+//   rightAnswer: "answerB",
 
-  question: "Dans quelle région de la France se trouve Lille ? ",
+//   question: "Dans quelle région de la France se trouve Lille ? ",
 
-  answers: {
-    answerA: "Île-de-France",
-    answerB: "Bretagne",
-    answerC: "Haut-de-France",
-    answerD: "Occitanie",
-  },
-  rightAnswer: "answerC",
+//   answers: {
+// answerA: "Île-de-France",
+// answerB: "Bretagne",
+// answerC: "Haut-de-France",
+// answerD: "Occitanie",
+//   },
+//   rightAnswer: "answerC",
 
-  question: "Quel est le nom du fleuve qui traverse Lille ?",
+//   question: "Quel est le nom du fleuve qui traverse Lille ?",
 
-  answers: {
-    answerA: "La Seine",
-    answerB: "La Loire",
-    answerC: "La Deûle",
-    answerD: "La Garonne",
-  },
-  rightAnswer: "answerC",
+//   answers: {
+// answerA: "La Seine",
+// answerB: "La Loire",
+// answerC: "La Deûle",
+// answerD: "La Garonne",
+//   },
+//   rightAnswer: "answerC",
 
-  question:
-    "Quel est le nom de la célèbre place de Lille, où se trouve l'Hôtel de Ville ?",
+//   question:
+// "Quel est le nom de la célèbre place de Lille, où se trouve l'Hôtel de Ville ?",
 
-  answers: {
-    answerA: "Place de la République",
-    answerB: "Place du Général de Gaulle (Grand-Place)",
-    answerC: "Place de la Bastille",
-    answerD: "Place de la Concorde",
-  },
-  rightAnswer: "answerB",
+//   answers: {
+// answerA: "Place de la République",
+// answerB: "Place du Général de Gaulle (Grand-Place)",
+// answerC: "Place de la Bastille",
+// answerD: "Place de la Concorde",
+//   },
+//   rightAnswer: "answerB",
 
-  question:
-    "Combien de départements composent la région des Hauts-de-France, où se trouve Lille ?",
+//   question:
+// "Combien de départements composent la région des Hauts-de-France, où se trouve Lille ?",
 
-  answers: {
-    answerA: "2",
-    answerB: "3",
-    answerC: "4",
-    answerD: "5",
-  },
-  rightAnswer: "answerB",
+//   answers: {
+// answerA: "2",
+// answerB: "3",
+// answerC: "4",
+// answerD: "5",
+//   },
+//   rightAnswer: "answerB",
 
-  question:
-    "Lille est située à proximité d'une frontière d'un pays voisin lequel ?",
+//   question:
+// "Lille est située à proximité d'une frontière d'un pays voisin lequel ?",
 
-  answers: {
-    answerA: "Belgique",
-    answerB: "Allemagne",
-    answerC: "Espagne",
-    answerD: "Italie",
-  },
-  rightAnswer: "answerA",
+//   answers: {
+// answerA: "Belgique",
+// answerB: "Allemagne",
+// answerC: "Espagne",
+// answerD: "Italie",
+//   },
+//   rightAnswer: "answerA",
 
-  question: "Quel est le nom de la Cathédrale de Lille ?",
+//   question: "Quel est le nom de la Cathédrale de Lille ?",
 
-  answers: {
-    answerA: "Notre Dame de la Garde",
-    answerB: "Notre Dame de la Treille",
-    answerC: "Notre Dame de l'Annonaal",
-    answerD: "La Cathédrale Sainte-Cécile",
-  },
-  rightAnswer: "answerB",
+//   answers: {
+// answerA: "Notre Dame de la Garde",
+// answerB: "Notre Dame de la Treille",
+// answerC: "Notre Dame de l'Annonaal",
+// answerD: "La Cathédrale Sainte-Cécile",
+//   },
+//   rightAnswer: "answerB",
 
-  question: "Quel surnom est encore aujourdhui donné à Lille ?",
+//   question: "Quel surnom est encore aujourdhui donné à Lille ?",
 
-  answers: {
-    answerA: "La capitale des Gaules",
-    answerB: "La belle endormie",
-    answerC: "La cité des Papes",
-    answerD: "La captale des Flandres",
-  },
-  rightAnswer: "answerD",
+//   answers: {
+// answerA: "La capitale des Gaules",
+// answerB: "La belle endormie",
+// answerC: "La cité des Papes",
+// answerD: "La captale des Flandres",
+//   },
+//   rightAnswer: "answerD",
 
-  question: "Quelle est l'adresse de la WCS Lille?",
+//   question: "Quelle est l'adresse de la WCS Lille?",
 
-  answers: {
-    answerA: "13 rue du Général de Gaule",
-    answerB: "124 avenue Montebello",
-    answerC: "2 rue Victor Hugo",
-    answerD: "on ne sait pas on s'est tous perdu a la rentrée",
-  },
-  rightAnswer: "answerD",
+//   answers: {
+// answerA: "13 rue du Général de Gaule",
+// answerB: "124 avenue Montebello",
+// answerC: "2 rue Victor Hugo",
+// answerD: "on ne sait pas on s'est tous perdu a la rentrée",
+//   },
+//   rightAnswer: "answerD",
 
-  question: "Qui était le maire de Lille de 1973 à 2001 ?",
+//   question: "Qui était le maire de Lille de 1973 à 2001 ?",
 
-  answers: {
-    answerA: "Pierre Mauroy",
-    answerB: "Martine Aubry",
-    answerC: "Patrick Kanner",
-    answerD: "Augustin Cament",
-  },
-  rightAnswer: "answerA",
-};
+//   answers: {
+// answerA: "Pierre Mauroy",
+// answerB: "Martine Aubry",
+// answerC: "Patrick Kanner",
+// answerD: "Augustin Cament",
+//   },
+//   rightAnswer: "answerA",
+// };
