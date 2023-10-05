edb0bf2677a1d038ccad0c7ad797b758549630f7
Romain <98345704+Romlion@users.noreply.github.com>
2023-10-05 17:15:12 +0200

    I add Right and Wrong

--- index.js
+++ index.js
@@ -100,6 +100,67 @@ const questions = [
   },
 ];
 
+let currentQuestionIndex = 0;
+let correctAnswers = 0;
+
+const questionText = document.getElementById("question-text");
+const optionsList = document.getElementById("options-list");
+const nextQuestionButton = document.getElementById("next-question-button");
+const resultsContainer = document.getElementById("results-container");
+
+function displayQuestion() {
+  const currentQuestion = questions[currentQuestionIndex];
+  questionText.textContent = currentQuestion.question;
+  optionsList.innerHTML = "";
+
+  currentQuestion.options.forEach((option, index) => {
+    const listItem = document.createElement("li");
+    const optionRadio = document.createElement("input");
+    optionRadio.type = "radio";
+    optionRadio.name = "answer";
+    optionRadio.value = index;
+    optionRadio.id = `option${index}`;
+    const optionLabel = document.createElement("label");
+    optionLabel.textContent = option;
+    optionLabel.setAttribute("for", `option${index}`);
+    listItem.appendChild(optionRadio);
+    listItem.appendChild(optionLabel);
+    optionsList.appendChild(listItem);
+  });
+}
+function checkAnswer() {
+  const selectedOption = document.querySelector('input[name="answer"]:checked');
+  if (!selectedOption) {
+    alert("Veuillez sélectionner une réponse.");
+    return;
+  }
+  const userAnswer = parseInt(selectedOption.value);
+  const currentQuestion = questions[currentQuestionIndex];
+
+  if (userAnswer === currentQuestion.correctAnswer) {
+    correctAnswers++;
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < questions.length) {
+    displayQuestion();
+  } else {
+    showResults();
+  }
+}
+// Montrer le résultat a la fin du questionnaire
+function showResults() {
+  questionText.textContent = "Questionnaire terminé !";
+  optionsList.innerHTML = "";
+  nextQuestionButton.style.display = "none";
+  resultsContainer.textContent = `Score : ${correctAnswers} / ${questions.length}`;
+}
+nextQuestionButton.addEventListener("click", checkAnswer);
+// Afficher la première question au chargement de la page
+displayQuestion();
+
+// Récupérez l'élément div par son ID
 // Question n°1
 const question1 = [0];
 const rightWrong1 = document.querySelectorAll(".answer");
@@ -427,74 +488,3 @@ function changeWrongColor3() {
   rightWrong10[3].style.backgroundColor = "Red";
   wrongAnswer.play();
 }
-
-let currentQuestionIndex = 0;
-let correctAnswers = 0;
-
-const questionText = document.getElementById("question-text");
-const optionsList = document.getElementById("options-list");
-const nextQuestionButton = document.getElementById("next-question-button");
-const resultsContainer = document.getElementById("results-container");
-
-function displayQuestion() {
-  const currentQuestion = questions[currentQuestionIndex];
-  questionText.textContent = currentQuestion.question;
-  optionsList.innerHTML = "";
-
-  currentQuestion.options.forEach((option, index) => {
-    const listItem = document.createElement("li");
-    const optionRadio = document.createElement("input");
-    optionRadio.type = "radio";
-    optionRadio.name = "answer";
-    optionRadio.value = index;
-    optionRadio.id = `option${index}`;
-
-    const optionLabel = document.createElement("label");
-    optionLabel.textContent = option;
-    optionLabel.setAttribute("for", `option${index}`);
-
-    listItem.appendChild(optionRadio);
-    listItem.appendChild(optionLabel);
-    optionsList.appendChild(listItem);
-  });
-}
-
-function checkAnswer() {
-  const selectedOption = document.querySelector('input[name="answer"]:checked');
-
-  if (!selectedOption) {
-    alert("Veuillez sélectionner une réponse.");
-    return;
-  }
-
-  const userAnswer = parseInt(selectedOption.value);
-  const currentQuestion = questions[currentQuestionIndex];
-
-  if (userAnswer === currentQuestion.correctAnswer) {
-    correctAnswers++;
-  }
-
-  currentQuestionIndex++;
-
-  if (currentQuestionIndex < questions.length) {
-    displayQuestion();
-  } else {
-    showResults();
-  }
-}
-
-// Montrer le résultat a la fin du questionnaire
-
-function showResults() {
-  questionText.textContent = "Questionnaire terminé !";
-  optionsList.innerHTML = "";
-  nextQuestionButton.style.display = "none";
-  resultsContainer.textContent = `Score : ${correctAnswers} / ${questions.length}`;
-}
-
-nextQuestionButton.addEventListener("click", checkAnswer);
-
-// Afficher la première question au chargement de la page
-displayQuestion();
-
-// Récupérez l'élément div par son ID
